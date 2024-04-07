d5d90c1dd373fa3a3501a484e4645f32d72a22b5
Bharath Kumar Karimilla <karimillab@mail.sacredheart.edu>
2024-04-06 21:20:17 -0400

    class 4/5/24

---
+++ Class 1 - Class work.R
@@ -0,0 +1,48 @@
+v1 <- 4.5
+v2 <- 2
+v3 <- v1/v2
+v3
+v4 <- sqrt(v1)
+v4
+greet <- "Hello,"
+name <-" Bob"
+message <- paste(greet, name)
+message
+#printed Hellow world
+"Hellow World"
+
+#Integers
+x <- 2L
+typeof(x)
+
+#double
+y <- 2.5
+typeof(y)
+
+#complex
+z <- 2+3i
+typeof(z)
+
+# Basic Math
+a <- 5
+b <- 3
+c <- a + b
+d <- a - b
+e <- a * b
+f <- a / b
+
+4 <5
+10>100
+4==5
+
+result <- 4 < 5
+result
+typeof(result)
+result <- 4 < 5
+result
+typeof(result)
+result2 <- !(5>1)
+result2
+result | result2
+result & result2
+isTRUE(result)
